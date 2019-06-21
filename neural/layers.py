48005f38a1fbcdced63a172d9d63896f1cbde680
Jay <jay1999ke@gmail.com>
2019-06-21 17:22:56 +0300

    Add Layer abstract class

--- neural/layers.py
+++ neural/layers.py
@@ -1,6 +1,18 @@
 from autodiff.autotensor import autoTensor, Node
 import torch
 
-class layer(object):
+class Layer(object):
+    """Abstract class that is inherited by all types of layers""" 
 
-    pass
+    def __call__(self):
+        raise  NotImplementedError("Should have implemented this")
+    
+class Linear(Layer):
+
+    def __init__(self, input_dim, output_dim):
+        self.input_dim = input_dim
+        self.output_dim =  output_dim
+
+    # TODO:
+    def __call__(self):
+        raise  NotImplementedError("Should have implemented this")
