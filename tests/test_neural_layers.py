f18cc023989fcaed5bfe8093267f95f839d01705
Jay <jay1999ke@gmail.com>
2019-06-23 21:53:00 +0300

    [Tests] param.py and layers.py unittests

--- tests/test_neural_layers.py
+++ tests/test_neural_layers.py
@@ -5,10 +5,24 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 #imports below
-
+from autodiff.autotensor import autoTensor
+from neural.layers import Linear
+import torch
 
 class test_class_Linear(unittest.TestCase):
     def setup_method(self, method):
         print("\n%s:%s" % (type(self).__name__, method.__name__))
-    pass
-    #TODO
+
+    def test_init(self):
+        layer = Linear(6,1)
+        
+        assert layer.weight.size() == torch.empty(6,1).size()
+        assert layer.bias.size() == torch.empty(1,1).size()
+
+    def test_call(self):
+        layer = Linear(6,1)
+        X = autoTensor(torch.rand(5,6))
+
+        l_out = layer(X)
+
+        assert torch.equal(l_out.value, torch.mm(X.value,layer.weight.value) + layer.bias.value)
--- tests/test_neural_param.py
+++ tests/test_neural_param.py
@@ -5,16 +5,51 @@ myPath = os.path.dirname(os.path.abspath(__file__))
 sys.path.insert(0, myPath + '/../')
 
 #imports below
-
+from neural.param import Initializer, Weight
+from autodiff.autotensor import autoTensor
+import torch
 
 class test_class_Weight(unittest.TestCase):
     def setup_method(self, method):
         print("\n%s:%s" % (type(self).__name__, method.__name__))
-    pass
-    #TODO
 
+    def test_init(self):
+        weight= Weight((5,6))
+        assert isinstance(weight,autoTensor)
+        assert weight.value.size() == torch.empty((5,6)).size()
+
+        initer = Initializer("xavier")
+        weight= Weight((5,6),initializer=initer)
+
+        assert isinstance(weight,autoTensor)
+        assert weight.value.size() == torch.empty((5,6)).size()
+
+    def test_updade_weights(self):
+        
+        weight= Weight((5,6))
+        pre = weight.value.cpu().detach().clone()
+        weight.grad =autoTensor( torch.rand((5,6)))
+
+        weight.update_weights(learning_rate=0.01)
+
+        assert torch.equal(weight.value,pre - 0.01*weight.grad.value)
+
+
+        
 class test_class_Initializer(unittest.TestCase):
     def setup_method(self, method):
         print("\n%s:%s" % (type(self).__name__, method.__name__))
-    pass
-    #TODO
+
+    def test_createParam(self):
+        initer = Initializer("xavier")
+
+        weight = initer.createParam((5,6))
+
+        assert isinstance(weight,torch.Tensor)
+        assert weight.size() == torch.empty((5,6)).size()
+
+    def test_createParamStatic(self):
+        weight = Initializer.createParamStatic((5,6))        
+
+        assert isinstance(weight,torch.Tensor)
+        assert weight.size() == torch.empty((5,6)).size()
